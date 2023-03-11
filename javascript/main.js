cd6a8035052a8a2b68154f13b5a2c95d1cdeb573
phivu99 <100842950+phivu99@users.noreply.github.com>
2023-03-11 08:26:09 +0700

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -71,15 +71,56 @@
       $(this).toggleClass("click");
       $(".dashboard__sidebar").toggleClass("show");
       $(".dashboard").toggleClass("show");
+
+  
+      $(".dashboard__sidebar").toggleClass("side-style");
+      $(this).find(".dash-show").removeClass("feat-show");
     });
+
     $(".feat-btn").click(function () {
-      $("nav ul .feat-show").toggleClass("show");
+      $(this).find(".feat-show").toggleClass("show");
+      $("show").slideToggle();
     });
 
     $(".sidebar__menu li").click(function () {
       $(this).addClass("active").siblings().removeClass("active");
+      
     });
+
   };
+  // $(document).on("click", ".mobile-button-style2", function () {
+  //   $(this).toggleClass("active");
+  //   $("#main-nav-mobi").slideToggle();
+  // });
+  // $(document).on("click", "#main-nav-mobi .arrow", function () {
+  //   $(this).toggleClass("active").next().slideToggle();
+  // });
+
+  // var delete_im = function (e) {
+  //   $(".feat-btn").on("click", function (e) {
+  //     e.preventDefault();
+  //     var $this = $(this);
+  //     $this.closest(".feat-show").toggleClass("show");
+  //   });
+  // };
+
+  // var delete_img = function (e) {
+  //   $(".remove-file").on("click", function (e) {
+  //     e.preventDefault();
+  //     var $this = $(this);
+  //     $this.closest(".file-delete").remove();
+  //   });
+  // };
+
+    /*---categories slideToggle---*/
+    $(".categories_title").on("click", function() {
+      $(this).toggleClass('active');
+      $('.categories_menu_toggle').slideToggle('medium');
+  }); 
+  $(".categories_title2").on("click", function() {
+    $(this).toggleClass('active');
+    $('.categories_menu_toggle22').slideToggle('medium');
+}); 
 
   var accordion = function () {
     if ($("div").hasClass("widget-accordion")) {
@@ -242,6 +283,13 @@
           nextEl: ".swiper-button-next3",
           prevEl: ".swiper-button-prev3",
         },
+        // breakpoints: {
+        //   600: {
+        //       slidesPerView: 2,
+        //       spaceBetween: 17,
+        //   },
+
+        // },
       });
   
       var thumbs = new Swiper(".gallery-thumb", {
@@ -250,6 +298,20 @@
         loop: true,
         slideToClickedSlide: true,
         slidesPerView: 3,
+        breakpoints: {
+          200: {
+            slidesPerView: 2,
+            spaceBetween: 17,
+        },
+          500: {
+              slidesPerView: 2,
+              spaceBetween: 17,
+          },
+          700: {
+              slidesPerView: 3,
+              spaceBetween: 17,
+          },
+        },
       });
       slider.controller.control = thumbs;
       thumbs.controller.control = slider;
@@ -463,40 +525,18 @@ var loadmore2 = function () {
     }
   };
 
+    
+
   var progressProduct = function () {
-    if ($(".progress-item").length > 0) {
-      $(".progress-item").appear(function () {
-          $(this).find("div.donat-bg").each(function () {
-              $(this).find(".custom-donat").delay(600).animate({
-                  width: $(this).attr("data-percent")
-              }, 1000, "easeInCirc");
-              var txt = $(this).attr("data-percent");
-              $(this).closest(".rating-details").find(".percent").text(txt);
-          });
-      });
-    }
-};
-
-var Modal_Right = function() {
-  const body = $('body');
-  const modalMenu = $('.sidebar-popup');
-  const modalMenuBody = modalMenu.children('.widget-filter');
-
-  if (modalMenu.length) {
-      const open = function() {
-          modalMenu.addClass('modal-menu--open');
-      };
-      const close = function() {
-          modalMenu.removeClass('modal-menu--open');
-      };
-
-      $('.button-filter').on('click', function() {
-          open();
-      });
-      $('.modal-menu__backdrop, .title-button-group').on('click', function() {
-          close();
-      });
-  }
+    $(".progress-item").appear(function () {
+        $(this).find("div.donat-bg").each(function () {
+            $(this).find(".custom-donat").delay(600).animate({
+                width: $(this).attr("data-percent")
+            }, 1000, "easeInCirc");
+            var txt = $(this).attr("data-percent");
+            $(this).closest(".rating-details").find(".percent").text(txt);
+        });
+    });
 };
 
   var preload = function () {
@@ -517,7 +557,6 @@ var Modal_Right = function() {
     dropOptionForm();
     flatProgressBar();
     accordion();
-    Modal_Right();
     goTop();
     mobileNav();
     counter();
@@ -541,6 +580,7 @@ var Modal_Right = function() {
     loadmore2();
     UpImg();
     delete_img();
+    delete_im();
     popUpLightBox();
     progressProduct();
     preload();
